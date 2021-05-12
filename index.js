d5f0009f27270fc9aa90af859eddd5300becb087
Jonathan Montoya <montoya0396@gmail.com>
2021-05-12 13:13:38 -0400

    changed some styles, added functionality behind adding a passenger to a route

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const ejsMate = require('ejs-mate')
 const session = require('express-session');
+const catchAsync = require('./utils/catchAsync')
+const ExpressError = require('./utils/ExpressError')
 // const passport = require('passport');
 // const LocalStrategy = require('passport-local');
 
-const app = express();
 
 
-const User = require('./models/user')
 const Route = require("./models/route")
 const Employee = require("./models/employee");
-const { json } = require("express");
 
 mongoose.connect("mongodb://localhost:27017/transportationSystem", 
 {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
 })
 
 const db = mongoose.connection;
@@ -27,6 +28,8 @@ db.once("open", () =>
     console.log("Database connected");
 })
 
+const app = express();
+
 // app.use(passport.initialize());
 // app.use(passport.session());
 // passport.use(new LocalStrategy(User.authenticate()))
@@ -34,14 +37,16 @@ db.once("open", () =>
 // passport.serializeUser(User.serializeUser());
 // passport.deserializeUser(User.deserializeUser())
 
+
+app.engine('ejs', ejsMate)
 app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true}));
-app.use(express.json())
 app.use(session({secret: 'loginSecret'}))
+
 
 const requireLogin = (req, res, next) =>
 {
@@ -62,7 +67,7 @@ app.get("/", loggedIn, (req, res) =>
     res.render("login");
 })
 
-app.post("/", async (req, res) =>
+app.post("/", catchAsync(async (req, res) =>
 {
     const { username, password } = req.body;
     const foundUser = await Employee.findAndValidate(username, password);
@@ -77,7 +82,7 @@ app.post("/", async (req, res) =>
     {
         res.redirect('/');
     }
-})
+}))
 
 app.post('/logout', (req, res) =>
 {
@@ -90,17 +95,13 @@ app.post('/logout', (req, res) =>
 app.get("/home", requireLogin, (req, res) =>
 {
     const userFound = req.session.user;
-    const title = userFound.title;
-    const lastName = userFound.surname;
     
-    res.render("home", { title, lastName });
+    res.render("home", { userFound });
 })
 
-app.get("/routes", requireLogin, async (req, res) =>
+app.get("/routes", requireLogin, catchAsync(async (req, res) =>
 {
     const userFound = req.session.user;
-    const title = userFound.title;
-    const lastName = userFound.surname;
 
     const routeA = await Route.find({ departureTime: "8:30AM"});
     const routeB = await Route.find({ departureTime: "9:00AM"});
@@ -109,31 +110,69 @@ app.get("/routes", requireLogin, async (req, res) =>
     const routeE = await Route.find({ departureTime: "4:30PM"});
     const routeF = await Route.find({ departureTime: "5:00PM"});
     
-    res.render("routes", { routeA, routeB, routeC, routeD, routeE, routeF, title, lastName} );
-})
+    res.render("routes", { routeA, routeB, routeC, routeD, routeE, routeF, userFound} );
+}))
 
 app.get("/schedule", requireLogin, (req, res) =>
 {
     const userFound = req.session.user;
-    const title = userFound.title;
-    const lastName = userFound.surname;
 
-    const routesFound = [];
-    res.render("schedule", { routesFound, title, lastName });
+    const routesA = [];
+    const routesB = [];
+    res.render("schedule", { routesA, routesB, userFound });
 })
 
-app.post("/schedule", async (req, res) =>
+app.post("/schedule", catchAsync(async (req, res) =>
 {
     const userFound = req.session.user;
-    const title = userFound.title;
-    const lastName = userFound.surname;
     
     const { from, to } = req.body;
     const routesFound = await Route.findAndCreate(from, to);
-    res.render("schedule", { routesFound, title, lastName });
+    const routesA = routesFound[0];
+    const routesB = routesFound[1];
+
+    res.render("schedule", { routesA, routesB, userFound });
     
+}))
+
+app.post("/scheduleRoute", catchAsync(async (req, res) =>
+{
+    const userFound = req.session.user;
+    const { routeA, routeB } = req.body;
+
+    let _id = routeA;
+    let route = await Route.findById({ _id })
+    let numPassengers = route.numPassengers + 1;
+
+    await Route.findByIdAndUpdate(_id, { numPassengers })
+    await route.passengers.push(userFound);
+    await route.save()
+
+    if (routeB)
+    {
+        _id = routeB;
+        route = await Route.findById({ _id })
+        numPassengers = route.numPassengers + 1;
+
+        await Route.findByIdAndUpdate(_id, { numPassengers })
+        await route.passengers.push(userFound);
+        await route.save()
+    }
+    
+    res.redirect("/schedule")
+}))
+
+app.all('*', (req, res, next) =>
+{
+    next(new ExpressError("Page Not Found", 404))
 })
 
+app.use((err, req, res, next) =>
+{
+    const {statusCode = 500} = err;
+    if (!err.message) err.message = "Oh no, something went wrong";
+    res.status(statusCode).render('error', { err });
+})
 
 
 
@@ -142,3 +181,4 @@ app.listen (3000, () =>
 {
     console.log("serving on port 3000");
 })
+
--- models/route.js
+++ models/route.js
@@ -4,13 +4,18 @@ const Schema = mongoose.Schema;
 
 const RouteSchema = new Schema(
 {
-    
     from: String,
     to: String,
     departureTime: String,
     arrivalTime: String,
     duration: Number,
-    numPassengers: Number
+    numPassengers: Number,
+    passengers: [
+    {
+        type: Schema.Types.ObjectId,
+        ref: 'Employee',
+        unique: true
+    }]
 })
 
 RouteSchema.statics.findAndCreate = async function(from, to)
@@ -18,24 +23,26 @@ RouteSchema.statics.findAndCreate = async function(from, to)
     let routesFound = await this.find({ from, to });
     let routesA, routesB;
     let altRoutes = [];
+
     if (routesFound.length === 0)
     {
         routesA = await this.find({from, to: "Midtown East"});
         routesB = await this.find({from: "Midtown East", to});
 
-        for (let route of routesA)
-        {
-            altRoutes.push(route);
-        }
-
-        for (let route of routesB)
-        {
-            altRoutes.push(route);
-        }
+        altRoutes.push(routesA);
+        altRoutes.push(routesB);
 
         return altRoutes
     }
-    return routesFound;
+
+    let mainRoutes = [];
+    mainRoutes.push(routesFound);
+    mainRoutes.push([]);
+
+    return mainRoutes;
 }
+
+
+
 
 module.exports = mongoose.model("Route", RouteSchema);
